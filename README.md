e2a184e20883d5b0b47c83fd3be7ce23d7ca2dee
Matt Valentine-House <matt@eightbitraptor.com>
2014-10-29 16:25:13 +0000

    Initial Commit
    A Skeleton cookbook generated using the ChefDK with
    
            chef generate cookbook chef-rustlang
    
    Although I have renamed the generated cookbook name to just 'rustlang' so
    that it looks better in the Supermarket later.

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef-rustlang
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'rustlang'
+maintainer       'Matt Valentine-House'
+maintainer_email 'matt@eightbitraptor.com'
+license          'all_rights'
+description      'Installs/Configures rustlang'
+long_description 'Installs/Configures rustlang'
+version          '0.1.0'
+
