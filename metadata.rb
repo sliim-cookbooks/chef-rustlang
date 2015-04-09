20bf961517b2a9f953225234827b458ac60f8b38
Matt Valentine-House <matt@eightbitraptor.com>
2015-04-09 17:34:43 +0100

    Update metadata.rb
    * Pull in the long description from the README.
    * Explicitly define supported operating systems.

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,11 @@ maintainer       'Matt Valentine-House'
 maintainer_email 'matt@eightbitraptor.com'
 license          'GPL v3'
 description      'Installs/Configures rustlang'
-long_description 'Installs/Configures rustlang'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.2'
 
 source_url 'https://github.com/eightbitraptor/chef-rustlang'
 issues_url 'https://github.com/eightbitraptor/chef-rustlang/issues'
 
+supports 'ubuntu', '>= 12.04'
+supports 'centos', '>= 5.11'
