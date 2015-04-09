9821619435b3a2e34b73ee43d6e65f808cd657f4
Matt Valentine-House <matt@eightbitraptor.com>
2015-04-09 17:28:11 +0100

    Release 0.1.2
    with CentOS Support

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'matt@eightbitraptor.com'
 license          'GPL v3'
 description      'Installs/Configures rustlang'
 long_description 'Installs/Configures rustlang'
-version          '0.1.1'
+version          '0.1.2'
 
 source_url 'https://github.com/eightbitraptor/chef-rustlang'
 issues_url 'https://github.com/eightbitraptor/chef-rustlang/issues'
