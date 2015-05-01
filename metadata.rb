f49be3e18a87acc9cb848fe313f5b515b0ff8f0f
Matt Valentine-House <matt@eightbitraptor.com>
2015-05-01 08:31:30 +0100

    Bump Version
    This release fixes the metadata displayed on the Chef Supermarket

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'matt@eightbitraptor.com'
 license          'GPL v3'
 description      'Installs/Configures rustlang'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 source_url 'https://github.com/eightbitraptor/chef-rustlang'
 issues_url 'https://github.com/eightbitraptor/chef-rustlang/issues'
